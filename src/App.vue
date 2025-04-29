5a13ad5770ab03468316e26727df54ac1ba2fe28
eunhye17 <wjddmsgo45@naver.com>
2025-04-30 00:20:24 +0900

    [add] Toast 추가

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,12 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <Toast />
   <RouterView />
 </template>
+
+<style>
+.p-toast {
+  z-index: 9999 !important;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -7,9 +7,14 @@ import PrimeVue from 'primevue/config'
 import 'primevue/resources/themes/saga-blue/theme.css'
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
+import ToastService from "primevue/toastservice";
+import Toast from "primevue/toast";
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(PrimeVue)
+app.use(ToastService)
+app.component('Toast', Toast)
 app.mount('#app')
